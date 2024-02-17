debea85e6cf2aa5d1f3e516a5b87bda910b91daf
Tinon Turja Majumder <tinonturjamajumder@Tinons-MacBook-Air.local>
2024-02-17 13:55:08 +0600

    related to numbers

--- selfpractice/src/selfpractice/self_practice_fibonacci.java
+++ selfpractice/src/selfpractice/self_practice_fibonacci.java
@@ -1,10 +1,27 @@
 package selfpractice;
+import java.util.Scanner;
 
 public class self_practice_fibonacci {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+Scanner input=new Scanner(System.in);
+System.out.print("enter the number upto which term you want to see the value of fibonacci \n");
+int num=input.nextInt();
+int a=0;
+int b=1;
+int c=0;
+int i;
+for(i=0;i<=num;i++) {
+	c=a+b;
+	
+	a=b;
+	b=c;
+	System.out.println(c);
+	
+}
+System.out.println("your final  number is :"+c);
 	}
 
 }
